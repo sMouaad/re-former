f27feeb903289666fd6bee9b6071d415ad105122
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-10-05 14:24:07 +0100

    feat: add #show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,22 @@
 class UsersController < ApplicationController
   def new
-    def new
-      @user = User.new
-    end
+   @user = User.new
   end
   def create
     user = User.new(user_params)
     if user.save
-      redirect_to new_user_path
+      redirect_to @user
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [ :create, :new ]
+  resources :users, only: [ :create, :new, :edit, :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
