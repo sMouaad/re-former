89016a353fa3a0cd0e70e616b420c51f469c7ed0
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-10-05 13:12:29 +0100

    fix: change name attribute to user[attribute] and add logic to #create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,16 @@ class UsersController < ApplicationController
   def new
   end
   def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
