6c36db0604b8bf4fcd048496483e9d9503e6988c
sMouaad <93816869+sMouaad@users.noreply.github.com>
2024-10-05 14:32:12 +0100

    fix: change variable user to @user in #create method (caused bugs)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
    @user = User.new
   end
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       redirect_to @user
     else
       render :new, status: :unprocessable_entity
